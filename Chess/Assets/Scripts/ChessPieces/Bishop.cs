d541d974b402f8fbbc8243276e85f8c30d2a6b6c
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-08-15 23:50:41 +0300

    Create bishop

--- Chess/Assets/Scripts/ChessPieces/Bishop.cs
+++ Chess/Assets/Scripts/ChessPieces/Bishop.cs
@@ -1,18 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Bishop : MonoBehaviour
+public class Bishop : Piece
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Square> _attackTurns;
+
+    public override List<Square> GetPossibleAttackTurns(Square squareWithThis)
     {
-        
+        return _attackTurns;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override List<Square> GetPossibleMoveTurns(Square squareWithThis)
     {
-        
+        _attackTurns = new List<Square>();
+
+        int x = squareWithThis.Coordinates.x;
+        int y = squareWithThis.Coordinates.y;
+
+        List<Square> turns = new List<Square>();
+
+        Vector2Int upDir = new Vector2Int(1, 1);
+        Vector2Int downDir = new Vector2Int(1, -1);
+        Vector2Int rightDir = new Vector2Int(-1, 1);
+        Vector2Int leftDir = new Vector2Int(-1, -1);
+
+        AddPossibleTurnsInDiagonal(turns, squareWithThis.Coordinates, upDir);
+        AddPossibleTurnsInDiagonal(turns, squareWithThis.Coordinates, downDir);
+        AddPossibleTurnsInDiagonal(turns, squareWithThis.Coordinates, rightDir);
+        AddPossibleTurnsInDiagonal(turns, squareWithThis.Coordinates, leftDir);
+
+        return turns;
+    }
+
+    private void AddPossibleTurnsInDiagonal(List<Square> turns, Vector2Int pieceCoordinats, Vector2Int rowDirection)
+    {
+        for (int i = 1; i < Chessboard.Instance.Length; i++)
+        {
+            Square square = _squareHandler.GetSquareWithCoordinates(pieceCoordinats.x + i * rowDirection.x, pieceCoordinats.y + i * rowDirection.y);
+
+            if (square == _squareHandler.GhostSquare)
+                break;
+
+            if (IsPieceStandsOnSquare(square))
+            {
+                if (IsPieceOnSquareHasOppositeColor(square))
+                {
+                    _attackTurns.Add(square);
+                    break;
+                }
+                else
+                    break;
+            }
+
+            turns.Add(square);
+        }
     }
 }
