9cbb1650faa84505d9ca60a33d9bc813f0a286a8
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-08-16 00:06:30 +0300

    Add test scenario for chesspieces

--- Chess/Assets/Scripts/ChessPieceSetuper.cs
+++ Chess/Assets/Scripts/ChessPieceSetuper.cs
@@ -5,8 +5,14 @@ public class ChessPieceSetuper : MonoBehaviour
 {
     public SquareHandler SquareHandler;
     public AllPiecesStorage PieceStorage;
-    public Piece Black;
-    public Piece White;
+    public Piece wq;
+    public Piece bq;
+    public Piece wp;
+    public Piece bp;
+    public Piece wk;
+    public Piece bk;
+    public Piece wr;
+    public Piece br;
 
     private Vector3 _offset;
 
@@ -18,10 +24,16 @@ public class ChessPieceSetuper : MonoBehaviour
     private IEnumerator Start()
     {
         yield return new WaitForSeconds(0.1f);
-        PieceStorage.AddPieceInArrayOfAllPieces(White);
-        PieceStorage.AddPieceInArrayOfAllPieces(Black);
-        SetChessPieceOnCell(White, 1, 1);
-        SetChessPieceOnCell(Black, 2, 2);
+        PieceStorage.AddPieceInArrayOfAllPieces(bq);
+        PieceStorage.AddPieceInArrayOfAllPieces(wq);
+        SetChessPieceOnCell(bq, 1, 1);
+        SetChessPieceOnCell(wq, 4, 1);
+        SetChessPieceOnCell(bp, 3, 5);
+        SetChessPieceOnCell(wp, 2, 7);
+        SetChessPieceOnCell(bk, 4, 4);
+        SetChessPieceOnCell(wk, 5, 4);
+        SetChessPieceOnCell(br, 5, 1);
+        SetChessPieceOnCell(wr, 6, 2);
     }
 
     private void SetChessPieceOnCell(Piece chessPiece, int x, int y)
