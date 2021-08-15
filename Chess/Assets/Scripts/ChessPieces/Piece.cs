cb1efd4c4d89acc11c5107fe6f9b7afa3a4afbfe
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-08-15 23:20:03 +0300

    Move 'ColorData' into 'Piece'

--- Chess/Assets/Scripts/ChessPieces/Piece.cs
+++ Chess/Assets/Scripts/ChessPieces/Piece.cs
@@ -81,3 +81,30 @@ public abstract class Piece : MonoBehaviour
         _gameManager = GameManager.Instance;
     }
 }
+
+[System.Serializable]
+public class ColorData
+{
+    public PieceColor Color { get { return _color; } }
+    [SerializeField] private PieceColor _color;
+
+    public int Multiplier
+    {
+        get
+        {
+            if (_color == PieceColor.Black)
+                return -1;
+            else if (_color == PieceColor.White)
+                return 1;
+            return 0;
+        }
+    }
+    private int _colorMultiplier;
+}
+
+[System.Serializable]
+public enum PieceColor
+{
+    Black,
+    White
+}
