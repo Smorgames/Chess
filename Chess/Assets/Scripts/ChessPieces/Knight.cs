3875236aa987f42ae4b2d2b7aad9cae5a5d1d7e8
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-08-15 22:07:09 +0300

    Create 'Knight' script

---
+++ Chess/Assets/Scripts/ChessPieces/Knight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : Piece
+{
+    public override List<Square> GetPossibleAttackTurns(Square squareWithThis)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<Square> GetPossibleMoveTurns(Square squareWithThis)
+    {
+        throw new System.NotImplementedException();
+    }
+}
