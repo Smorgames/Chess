95dae34b65f621656d2971f59352fc96772c37ee
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-11-10 17:22:21 +0300

    Add restart button and whose turn display

---
+++ Chess/Assets/Scripts/Managers/GameCommands.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameCommands : MonoBehaviour
+{
+    public void RestartGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
---
+++ Chess/Assets/Scripts/WhoseTurnAnimation.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class WhoseTurnAnimation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _tmp;
+
+    private void ChangeTMP(string whoseTurnCode)
+    {
+        var text = whoseTurnCode == "w" ? "White" : "Black";
+        var color = whoseTurnCode == "w" ? Color.white : Color.black;
+
+        _tmp.color = color;
+        _tmp.text = $"{text}'s turn!";
+    }
+    
+    private void OnTurnOrderChanged(object sender, TurnOrderEventArgs e)
+    {
+        var whoseTurn = Game.Instance.WhoseTurn;
+        ChangeTMP(whoseTurn);
+    }
+
+    private void Start()
+    {
+        Game.OnTurnOrderChanged += OnTurnOrderChanged;
+    }
+
+    private void OnDestroy()
+    {
+        Game.OnTurnOrderChanged -= OnTurnOrderChanged;
+    }
+}
