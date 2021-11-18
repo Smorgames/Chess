dac699e31ac28588d624a03d57a83b62c9e26d3b
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-11-18 16:19:41 +0300

    Added water pieces flickers

---
+++ Chess/Assets/Scripts/PawnRandomFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnRandomFire : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _pawnFirePrefabs;
+
+    private Vector3 _offset = new Vector3(0f, 0.025f, 0f);
+
+    private void Start()
+    {
+        SetRandomPawnFire();
+    }
+
+    private void SetRandomPawnFire()
+    {
+        var i = Random.Range(0, _pawnFirePrefabs.Count);
+        var fire = Instantiate(_pawnFirePrefabs[i], Vector3.zero, Quaternion.identity);
+        fire.transform.parent = gameObject.transform;
+        fire.transform.localPosition = Vector3.zero + _offset;
+    }
+}
